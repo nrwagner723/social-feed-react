dd313a9b84fca92d80cb4d223da58539cb53a996
Natalie Wagner <nrwagner723@gmail.com>
2022-07-20 21:44:34 -0500

    created navbar and made css file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import Posts from './Components/Posts/Posts';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
 
   const [entries, setEntries] = useState([{name: 'David Lagrange', post: 'I recently went into the woods to search for the oldest tree I could find. To my luck, I found a very old hemlock around 200 - 250 years old. Let me know if you would like me to show it to you!', date: '07-20-2022'}])
 
   function addNewEntry(entry) {
-    let tempEntries = [entry, ...entries];
+    let tempEntries = [...entries, entry];
     setEntries(tempEntries);
   }
 
   return (
-    <div>
-      <DisplayEntries parentEntries={entries} />
-      <Posts addNewEntryProperty={addNewEntry} />
+    <div className='container-fluid'>
+      <NavBar />
+      <div className='row'>
+        <div className='col-md-6'>
+          <Posts addNewEntryProperty={addNewEntry} />
+        </div>
+        <div className='col-md-6'>
+          <DisplayEntries parentEntries={entries} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return ( 
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <span class="navbar-text">Social<small>Feed</small></span>
+            </div>
+        </nav>
+     );
+}
+
+export default NavBar;
