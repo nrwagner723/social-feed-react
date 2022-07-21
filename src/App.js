a262a8f4b4c68325c6a77859356297b7b9e7fae0
Natalie Wagner <nrwagner723@gmail.com>
2022-07-20 20:20:35 -0500

    started making table to hold the name, post, and date

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
 
 function App() {
+
+  const [entries, setEntries] = useState([{name: 'David Lagrange', post: 'I recently went into the woods to search for the oldest tree I could find. To my luck, I found a very old hemlock around 200 - 250 years old. Let me know if you would like me to show it to you!', date: '07-20-2022'}])
+
   return (
     <div>
-      <h3>hello</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Post</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry) => {
+            return (
+              <tr>
+                <td>{entry.name}</td>
+                <td>{entry.post}</td>
+                <td>{entry.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
